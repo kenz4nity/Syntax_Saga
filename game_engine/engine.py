0cbd3b9a5aa44c95a7fade99ace8432c210eefa8
Efren Lambenecio <elambenecio07@gmail.com>
2026-01-12 13:55:36 +0800

    JSON file content has 5 lessons(due to time constraint but I can still add more if want nyo )
    Finalized engine.py with answer checking and lesson progression logic.
    
    lmk if may tanong kau

---
+++ game_engine/engine.py
@@ -0,0 +1,45 @@
+import json
+import os
+
+class AdventureEngine: 
+    
+    def __init__(self, content_path='game_engine/adventure_content.json'):
+        """initializing the engine and loading the story content from a JSON file."""
+        self.content_path = content_path
+        self.lessons = self.load_content()
+        self.current_lesson_key = "Introduction to Python"
+
+    def load_content(self):
+        """reads the JSON file created"""
+        try: 
+            with open(self.content_path, 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print("Error: adventure_content.json file not found.")
+            return{}
+        
+    def get_current_lesson(self):
+        """ retrieves the current lesson based on the current lesson key."""
+        return self.lessons.get(self.current_lesson_key, {})
+    
+    def check_answer(self, user_output):
+        """compares the user's output with the expected output for the current lesson."""
+        lesson = self.get_current_lesson()
+        expect_output = lesson.get("expected_output", "").strip()
+
+        #.strip() is used to ignore accidental extra spaces or newlines
+        if user_output.strip() == expect_output:
+            return True, lesson.get("success_msg")    
+        else:
+            return False, lesson.get("error_hint")
+
+    def move_to_next_lesson(self):
+        """updates the progress tracker to the next lesson in the sequence."""
+        lesson_keys = list(self.lessons.keys())
+        current_index = lesson_keys.index(self.current_lesson_key)
+
+        if current_index < len(lesson_keys) - 1:
+            self.current_lesson_key = lesson_keys[current_index + 1]
+            return True
+        return False # No more lessons available
+       
